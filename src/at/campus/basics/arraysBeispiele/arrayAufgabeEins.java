9ae66bcd1a7417a5e3876fbc5df3ee4296e93994
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-03-11 19:59:13 +0100

    init

---
+++ src/at/campus/basics/arraysBeispiele/arrayAufgabeEins.java
@@ -0,0 +1,19 @@
+package at.campus.basics.arraysBeispiele;
+
+import java.util.stream.IntStream;
+
+public class arrayAufgabeEins {
+
+    public static void main(String[] args) {
+
+        // Erstelle ein Array von 10 ganzen Zahlen. Gehe das Array in einem Loop durch und berechne die Summe!
+
+        int [] numbers = {1, 2, 3, 4, 5, 6 ,7 ,8, 9, 10};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        int sum = IntStream.of(numbers).sum();
+        System.out.println("The sum is: " + sum);
+    }
+}
