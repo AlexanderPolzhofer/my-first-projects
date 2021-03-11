6022d2d7534ff8ec3053339918b7e7e2a8b6eb58
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-03-11 18:50:02 +0100

    bankomat update

--- src/at/campus/basics/bankomat/Bankomat.java
+++ src/at/campus/basics/bankomat/Bankomat.java
@@ -5,56 +5,67 @@ import java.util.Scanner;
 public class Bankomat {
     public static void main(String[] args) {
         // Erstelle ein Programm, das einen Bankomaten simuliert. Folgende Möglichkeiten gibt es:
-        //1. Einzahlen
-        //2. Abheben
-        //3. Kontostand
-        //4. Beenden
+        // 1. Einzahlen
+        // 2. Abheben
+        // 3. Kontostand
+        // 4. Beenden
 
-
-
-        String[] bankomatProgramm = {"1. Einzahlen", "2. Abheben", "3. Kontostand", "4. Beenden"};
-
+//        boolean isAtmMachineRunning = true;
         int accountValue = 500;
-        //int selection = 0123;
 
-        //final int EINZAHLEN = 0;
-        //final int ABHEBEN = 1;
-       // final int KONTOSTAND = 2;
-       // final int BEENDEN = 3;
-
-        //final String[] optionAsString = {"Einzahlen", "Abheben", "Kontostand", "Beenden"};
+        while (true) {
+            String[] bankomatProgramm = {"Bitte wählen Sie eine Möglichkeit aus:", "1. Einzahlen", "2. Abheben", "3. Kontostand", "4. Beenden"};
 
 
-        for (String i : bankomatProgramm) {
-            System.out.println(i);
+            for (String i : bankomatProgramm) {
+                System.out.println(i);
+            }
+
+
+            Scanner scanner = new Scanner(System.in);
+            int typeInNumber = scanner.nextInt();
+            int depositNumber;
+            int withdrawMoney;
+
+
+
+            switch (typeInNumber) {
+                case 1:
+                    System.out.println("Wie viel € möchten Sie einzahlen?");
+                    depositNumber = scanner.nextInt();
+                    if (depositNumber < 0) {
+                        System.out.println("Bitte geben Sie einen gültigen Betrag ein!");
+                    } else {
+                        accountValue = accountValue + depositNumber;
+                        System.out.println("Ihr Kontostand beträgt nun " + (accountValue) + "€");
+
+                    }
+
+                    break;
+                case 2:
+                    System.out.println("Wie viel € möchten Sie abheben?");
+                    withdrawMoney = scanner.nextInt();
+                    if (withdrawMoney > accountValue) {
+                        System.out.println("Kontodeckung prüfen!");
+                    } else {
+                        accountValue = accountValue - withdrawMoney;
+                        System.out.println("Bitteschön, hier sind Ihre " + withdrawMoney + " €");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Ihr Kontostand beträgt: " + accountValue + " €");
+                    break;
+                case 4:
+                    System.out.println("Auf Wiedersehen, bis zum nächsten Mal!");
+//                        isAtmMachineRunning = false;
+                    System.exit(0);
+                        break;
+                default:
+                    System.out.println("Wählen Sie eine gültige Eingabe!");
+                    String reTypeIn = scanner.nextLine();
+                    System.out.println(reTypeIn);
+                    break;
+                    }
+            }
         }
-
-        System.out.println("Bitte wählen Sie eine Möglichkeit aus:");
-
-        Scanner scanner = new Scanner(System.in);
-        int typeInNumber = scanner.nextInt();
-        //String typeIn = scanner.nextLine();
-
-        switch (typeInNumber) {
-            case 0:
-                System.out.println("Wie viel € möchten Sie einzahlen?");
-                break;
-            case 1:
-                System.out.println("Wie viel € möchten Sie abheben?");
-                break;
-            case 2:
-                System.out.println("Ihr Kontostand beträgt: " + accountValue + " €");
-                break;
-            case 3:
-                System.out.println("Auf Wiedersehen, bis zum nächsten Mal!");
-                break;
-            default:
-                System.out.println("Wählen Sie eine gültige Eingabe!");
-                String reTypeIn = scanner.nextLine();
-                break;
-        }
-
-    }
-
 }
-
