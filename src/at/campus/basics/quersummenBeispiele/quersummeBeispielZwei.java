cbd50f8cc5282324ae40f77ebf6772b4e4d317b3
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-03-21 16:46:32 +0100

    zwei Möglichkeiten

--- src/at/campus/basics/quersummenBeispiele/quersummeBeispielZwei.java
+++ src/at/campus/basics/quersummenBeispiele/quersummeBeispielZwei.java
@@ -7,6 +7,8 @@ public class quersummeBeispielZwei {
         Beispiel: wenn die Quersumme 49 ergibt, ist dies 7 mal durch 7 teilbar
         * */
 
+        // Möglichkeit eins:
+
         int sumTwo = 0;
 
         for (int j = 0; j < 1000; j++) {
@@ -27,6 +29,29 @@ public class quersummeBeispielZwei {
         }
         System.out.println("Insgesamt sind es: " + sumTwo + " Durchläufe!");
 
+
+        // Möglichkeit zwei:
+
+        int countingSum = 0;
+
+        for (int i = 0; i <= 1000; i++) {
+
+            int numberOne = i;
+            int crossSum = 0;
+
+            while (numberOne > 0) {
+
+                int lastNumber = numberOne % 10;
+                numberOne = numberOne / 10;
+                crossSum += lastNumber;
+
+            }
+            if (crossSum % 7 == 0) {
+                countingSum++;
+                System.out.println("Zahl: " + i + " Die Quersumme beträgt: " + crossSum);
+            }
+        }
+        System.out.println("In Summe sind es: " + countingSum + " Durchläufe!");
     }
 }
 
