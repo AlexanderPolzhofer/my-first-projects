66f3fe35dc6d4b463c38d6e33c6a2ce5247953eb
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-03-21 16:16:09 +0100

    zwei Möglichkeiten

--- src/at/campus/basics/quersummenBeispiele/quersummeBeispielEins.java
+++ src/at/campus/basics/quersummenBeispiele/quersummeBeispielEins.java
@@ -4,9 +4,12 @@ public class quersummeBeispielEins {
 
     public static void main(String[] args) {
 
-        // Aufgabe Quersumme 1: Gib alle Zahlen zwischen 0 und 1000 aus, welche eine Quersumme von 15 haben.
-        // Beispiel: die Quersumme von 253 ist 2 + 5 + 3 = 10
+//         Aufgabe Quersumme 1: Gib alle Zahlen zwischen 0 und 1000 aus, welche eine Quersumme von 15 haben.
+//         Beispiel: die Quersumme von 253 ist 2 + 5 + 3 = 10
 
+        // Möglichkeit eins:
+
+        int sumTwo = 0;
 
         for (int j = 0; j < 1000; j++) {
             String numberAsString = Integer.toString(j);
@@ -22,7 +25,43 @@ public class quersummeBeispielEins {
 
             if (sum == 15) {
                 System.out.println("Durchlauf: " + j + " Quersumme: " + sum);
+                sumTwo++;
             }
         }
+        System.out.println("Insgesamt sind es: " + sumTwo + " Durchläufe!");
+
+        // Möglichkeit zwei:
+
+        int countingSum = 0;
+
+        for (int k = 0; k <= 1000; k++) {
+
+            int numberOne = k;
+            int crossSum = 0;
+
+            while (numberOne > 0) {
+                int lastNumber = numberOne % 10;
+                numberOne = numberOne / 10;
+                crossSum += lastNumber;
+
+            }
+            if (crossSum == 15) {
+                System.out.println("Zahlnummer: " + k + " mit der Quersumme von: " + crossSum);
+                countingSum++;
+            }
+        }
+        System.out.println("Insgesamt sind es: " + countingSum + " Durchläufe!");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
