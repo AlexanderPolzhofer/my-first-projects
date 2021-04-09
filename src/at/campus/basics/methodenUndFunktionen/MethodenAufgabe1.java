18679167acb109af580ce59e3864030cb64cc571
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-04-09 14:32:23 +0200

    init caesar

--- src/at/campus/basics/methodenUndFunktionen/MethodenAufgabe1.java
+++ src/at/campus/basics/methodenUndFunktionen/MethodenAufgabe1.java
@@ -1,41 +1,83 @@
 package at.campus.basics.methodenUndFunktionen;
 
+import java.util.Scanner;
+
 public class MethodenAufgabe1 {
+
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         // Eine Methode zur Subtraktion. Die Methode bekommt 2 Parameter (double) und liefert das Ergebnis der Subtraktion zurück.
 
-        System.out.println(minus());
+        printProgramMinus();
 
         // Eine Methode zur Multiplikation. Die Methode bekommt 2 Parameter (double) und liefert das Ergebnis der Multiplikation zurück.
 
-        System.out.println(multiply());
+        printProgramMultiply();
 
         // Eine Methode zur Division. Die Methode bekommt 2 Parameter (double) und liefert das Ergebnis der Division zurück.
 
-        System.out.println(division());
+        printProgramDivision();
+
+        // Eine Methode zur Addition. Die Methode bekommte 2 Parameter (double) und liefert das Ergebnis der Addition zurück.
+
+        printProgramAdding();
 
     }
 
-    public static double minus() {
-        double a = 3.1415;
-        double b = 0.918273;
+    public static void printProgramMinus() {
+        System.out.println("***Subtraktion***");
+        System.out.println("Bitte die erste Zahl eingeben: ");
+        double a = scan.nextDouble();
+        System.out.println("Bitte die zweite Zahl eingeben: ");
+        double b = scan.nextDouble();
+        System.out.println("Das Ergebnis beträgt: " + minus(a, b));
+    }
 
+    public static double minus(double a, double b) {
         return a - b;
     }
 
-    public static double multiply() {
-        double t = 514.41789;
-        double z = 87941325.4587912453;
+    public static void printProgramMultiply() {
+        System.out.println("***Multiplikation***");
+        System.out.println("Bitte die erste Zahl eingeben: ");
+        double a = scan.nextDouble();
+        System.out.println("Bitte die zweite Zahl eingeben: ");
+        double b = scan.nextDouble();
 
-        return t * z;
+        System.out.println("Das Ergebnis beträgt: " + multiply(a, b));
     }
 
-    public static double division() {
-        double p = 12.4789;
-        double q = 9.783102;
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
 
-        return p / q;
+    public static void printProgramDivision() {
+        System.out.println("***Division***");
+        System.out.println("Bitte die erste Zahl eingeben: ");
+        double a = scan.nextDouble();
+        System.out.println("Bitte die zweite Zahl eingeben: ");
+        double b = scan.nextDouble();
+
+        System.out.println("Das Ergebnis beträgt: " + division(a, b));
+    }
+
+    public static double division(double a, double b) {
+        return a / b;
+    }
+
+    public static void printProgramAdding() {
+        System.out.println("***Addition***");
+        System.out.println("Bitte die erste Zahl eingeben: ");
+        double a = scan.nextDouble();
+        System.out.println("Bitte die zweite Zahl eingeben: ");
+        double b = scan.nextDouble();
+
+        System.out.println("Das Ergebnis beträgt: " + adding(a, b));
+    }
+
+    public static double adding(double a, double b) {
+        return a + b;
     }
 }
