9128c2bfb6f5f5ad40a47acb1d27b86a07749a2b
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-05-02 18:37:53 +0200

    init exercise

--- src/at/campus/basics/arraysBeispiele/ArraysAufgabeVier.java
+++ src/at/campus/basics/arraysBeispiele/ArraysAufgabeVier.java
@@ -3,8 +3,11 @@ package at.campus.basics.arraysBeispiele;
 public class ArraysAufgabeVier {
 
     public static void main(String[] args) {
-        rectangle(4, 5);
-
+        rectangle(5, 21);
+        System.out.println();
+        multiplicationTablesWhileLoop(5);
+        System.out.println();
+        multiplicationTablesForLoop(6);
     }
 
     private static void rectangle(int i, int j) {
@@ -13,6 +16,28 @@ public class ArraysAufgabeVier {
                 System.out.print("X");
             }
             System.out.println();
+        }
+    }
+
+    private static void multiplicationTablesForLoop(int a) {
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int number : numbers) {
+            int result = a * number;
+            System.out.println(a + " * " + number + " = " + result);
+        }
+    }
+
+    private static void multiplicationTablesWhileLoop(int a) {
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int i = 0;
+
+        while (i < numbers.length) {
+            int result = a * numbers[i];
+            System.out.println(a + " * " + numbers[i] + " = " + result);
+            i++;
         }
     }
 
