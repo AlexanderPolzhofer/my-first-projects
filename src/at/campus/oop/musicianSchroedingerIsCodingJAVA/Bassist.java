30d22ac950500f1e11cbf47208dfc6bc3624d83c
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-05-24 18:20:37 +0200

    initMusicianSchroedingerCodesJAVA

--- src/at/campus/oop/musicianSchroedingerIsCodingJAVA/Bassist.java
+++ src/at/campus/oop/musicianSchroedingerIsCodingJAVA/Bassist.java
@@ -2,7 +2,7 @@ package at.campus.oop.musicianSchroedingerIsCodingJAVA;
 
 public class Bassist extends Musician {
     public Bassist(String preName, String name, int age, Band band) {
-        super(preName,name, age, band);
+        super(preName, name, age, band);
     }
 
     @Override
